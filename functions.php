b28e1676fce36293705bd21fad38f08ad8f63251
mauretto78 <assistenza@easy-grafica.com>
2016-04-27 20:15:48 +0200

    .

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// Load Eframework Kernel
+require 'framework/bootstrap.php';
+
+use Framework\Framework\WP\Admin\Admin;
+use Framework\Framework\WP\Admin\AdminPage;
+use Framework\Framework\WP\Enqueuer;
+use Framework\Framework\WP\Path;
+
+// enqueue styles
+$e = new Enqueuer();
+$e->addAdminStyle('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css', array(), '4.6.1', 'all');
+$e->addAdminStyle('framework-style', Path::template('/framework/admin/css/framework.css'), array(), '1.0.0', 'all');
+$e->enqueue();
+
+// admin pages
+$admin = new Admin();
+$admin->addPage(new AdminPage('eframework','E-Framework','edit_themes','base.php'));
