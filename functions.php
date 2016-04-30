2a5498794366e926c58be6ddca70b29a8d0f72d4
mauretto78 <assistenza@easy-grafica.com>
2016-04-30 08:42:02 +0200

    merging local master

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 // Load Eframework Kernel
 require 'framework/bootstrap.php';
 
@@ -12,9 +11,11 @@ use Framework\Framework\WP\Path;
 $e = new Enqueuer();
 $e->addAdminStyle('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css', array(), '4.6.1', 'all');
 $e->addAdminStyle('framework-style', Path::template('/framework/admin/css/framework.css'), array(), '1.0.0', 'all');
+$e->addAdminScript('media-upload-js', Path::template('/framework/admin/js/admin-media-upload.js'), array('jquery'), '1.0.0', true);
+$e->addAdminScript('ace-editor-js', Path::template('/framework/admin/assets/ace/src/ace.js'), array('jquery'), '1.0.0', true);
+$e->addAdminScript('framework-js', Path::template('/framework/admin/js/framework.js'), array('jquery'), '1.0.0', true);
 $e->enqueue();
 
 // admin pages
 $admin = new Admin();
-$admin->addPage(new AdminPage('eframework','E-Framework','edit_themes','base.php'));
-$admin->addPage(new AdminPage('eframework','E-Framework','edit_themes','base.php'));
+$admin->addPage(new AdminPage('eframework','E-Framework','edit_themes','layout.php'));
