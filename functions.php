7e41a6e2afb4c704fd33f5127cfe5c1440cffb1c
mauretto78 <assistenza@easy-grafica.com>
2016-07-04 00:06:24 +0200

    removed func folder

--- functions.php
+++ functions.php
@@ -2,22 +2,6 @@
 // Load Kernel and return $app
 require 'framework/bootstrap.php';
 
-// Admin
-require 'func/admin.php';
-
-// Custom Post type
-require 'func/cpt.php';
-
-// Enqueue styles and scripts
-require 'func/enqueue.php';
-
-// Nav menu
-require 'func/nav.php';
-
-// Shortcodes
-require 'func/shortcodes.php';
-
-// Widgets
-require 'func/widgets.php';
-
-
+//Loading Custom function
+$childTheme = $path::childDir('/inc/custom-functions.php');
+include $childTheme;
